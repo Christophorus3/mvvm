8782ca355449b438794149611a383c240075d220
christophwottawa <christoph.wottawa@me.com>
2019-05-31 14:29:48 +0200

    Created project

---
+++ mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm
+//
+//  Created by Christoph Wottawa on 31.05.19.
+//  Copyright © 2019 Christoph Wottawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
